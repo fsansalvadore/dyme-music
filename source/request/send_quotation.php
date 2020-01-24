d7994534169b55e89cd5f62c146df99899b4a57c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-01-24 18:40:45 +0100

    made modifications and fixes

--- source/request/send_quotation.php
+++ source/request/send_quotation.php
@@ -1,6 +1,6 @@
 <?php
 $errors = '';
-$myemail = 'f.sansalvadore@gmail.com';//<-----Put Your email address here.
+$myemail = 'dyme.music@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['tipologia']))
@@ -15,6 +15,7 @@ $tel = $_POST['tel'];
 $type = $_POST['tipologia'];
 $formation = $_POST['formation'];
 $message = $_POST['textarea1'];
+$altro = $_POST['altro'];
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
@@ -27,14 +28,15 @@ if( empty($errors))
 
 {
 $to = $myemail;
-$email_subject = "Richiesta preventivo da: $name";
+$email_subject = "DYME | Richiesta preventivo da: $name";
 $email_body = "Nuova richiesta di preventivo da: \n \n Nome: $name \n ".
 "Azienda: $business\n ".
 "Email: $email_address\n ".
 "Telefono: $tel\n ".
 "Tipologia di evento: $type\n ".
 "Formazione richiesta: $formation\n ".
-"Messaggio: \n $message";
+"Messaggio: \n $message\n".
+"Altro: \n $altro";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
